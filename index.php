1ffab0254d1ac087c87ddb24561565445b0e9ca6
Meryem Nazım Beyoğlu <162388011+MeryemNazimBeyoglu@users.noreply.github.com>
2024-09-10 10:56:43 +0300

    Update index.php

--- index.php
+++ index.php
@@ -34,16 +34,14 @@
         </form>
         <?php
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            // DATABASE connection
+             
             $servername = "localhost";
             $username = "vulnuser";
             $password = "password123";
             $dbname = "file_upload_db";
 
-            // Create connection
             $conn = new mysqli($servername, $username, $password, $dbname);
 
-            // Check connection
             if ($conn->connect_error) {
                 die("Connection failed: " . $conn->connect_error);
             }
@@ -53,7 +51,6 @@
             $uploadOk = 1;
             $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
-            // Check if image file is a actual image or fake image
             if (isset($_POST["submit"])) {
                 $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
                 if ($check !== false) {
@@ -76,7 +73,6 @@
                 $uploadOk = 0;
             }
 
-            // Allow certain file formats
             if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif") {
                 echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.<br>";
                 $uploadOk = 0;
